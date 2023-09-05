42a2f65e049a7e686bb24fb7a309daeaa807961b
naivete13sky <chen320821@163.com>
2023-09-05 16:55:08 +0800

    8 click

--- EPVS/Views/MainWindow.axaml.cs
+++ EPVS/Views/MainWindow.axaml.cs
@@ -1,5 +1,6 @@
-﻿using Avalonia.Controls;
-
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
 namespace EPVS.Views;
 
 public partial class MainWindow : Window
@@ -8,4 +9,36 @@ public partial class MainWindow : Window
     {
         InitializeComponent();
     }
+
+
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+
+
+
+    private void ShowPopupOnClick(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        // 创建一个弹窗
+        var dialog = new Window
+        {
+            Title = "后退弹窗",
+            Content = new TextBlock
+            {
+                Text = "后退按钮被点击！",
+                FontSize = 16,
+                HorizontalAlignment = Avalonia.Layout.HorizontalAlignment.Center,
+                VerticalAlignment = Avalonia.Layout.VerticalAlignment.Center,
+            },
+            Width = 300,
+            Height = 200,
+        };
+
+        // 显示弹窗
+        dialog.ShowDialog(this);
+    }
+
 }
+
