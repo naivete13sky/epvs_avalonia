bf79c5bca4468777bebde6704b4bf498d16f75bf
naivete13sky <chen320821@163.com>
2023-09-05 18:57:49 +0800

    14

--- EPVS/ViewModels/MainViewModel.cs
+++ EPVS/ViewModels/MainViewModel.cs
@@ -6,4 +6,14 @@ namespace EPVS.ViewModels;
 public class MainViewModel : ViewModelBase
 {
     public string Greeting => "Welcome to Avalonia!cc";
+
+
+
+    public ObservableCollection<string> Items { get; } = new ObservableCollection<string>
+    {
+        "桌面",
+        "下载",
+        "文档",
+        "c:/job"
+    };
 }
